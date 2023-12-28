5026812136bbecf1476f419f47ad5adfb664c057
Nurillo04 <aminjonovnurillo95@gmail.com>
2023-12-28 15:19:19 +0500

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,40 @@
 import React from "react";
+import { logo } from "../../assets";
 
 import "./Header.scss";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <>
+      <div className="header">
+        <div className="container">
+          <div className="header__wrapper">
+            <div className="header__left">
+              <img src={logo} alt="rasm" />
+            </div>
+            <div className="header__right">
+              <div className="header__right-nav">
+                <nav>
+                  <ul className="header__list">
+                    <div className="header__item">Продукция</div>
+                    <div className="header__item">Сертификаты</div>
+                    <div className="header__item">Наша команда</div>
+                    <div className="header__item">О нас</div>
+                    <div className="header__item">Новости</div>
+                    <div className="header__item">Вакансии</div>
+                    <div className="header__item">Контакты</div>
+                  </ul>
+                </nav>
+              </div>
+              <div className="header__right-btn">
+                <button className="header__btn btn1">RU</button>
+                <button className="header__btn btn2">EN</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
